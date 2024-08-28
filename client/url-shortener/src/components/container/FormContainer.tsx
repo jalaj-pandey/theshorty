6d26ad9465cc50a893a5fb796414fa906b00e6a9
Jalaj Pandey <62509462+jalaj-pandey@users.noreply.github.com>
2024-08-29 01:05:48 +0530

    Update FormContainer.tsx

--- client/url-shortener/src/components/container/FormContainer.tsx
+++ client/url-shortener/src/components/container/FormContainer.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import axios from "axios";
-import { serverUrl } from "../../helpers/Constant";
+import { serverUrl } from "../../helpers/Constant.ts";
 
 interface IFormContainerProps {
   updateReloadState: ()=> void;
