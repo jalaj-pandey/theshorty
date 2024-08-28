528161e04f21e2124ab5f5692b02a9281ba749d4
Jalaj Pandey <62509462+jalaj-pandey@users.noreply.github.com>
2024-08-28 22:46:49 +0530

    Update Constant.ts

--- client/url-shortener/src/helpers/Constant.ts
+++ client/url-shortener/src/helpers/Constant.ts
@@ -1,1 +1,1 @@
-export const serverUrl = import.meta.env.VITE_SERVER_URL || "http://localhost:5001/api"
+export const serverUrl = import.meta.env.VITE_SERVER_URL || "https://theshorty.onrender.com/api"
