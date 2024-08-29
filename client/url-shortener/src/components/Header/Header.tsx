7087debc12f496429464179e9c235b400f102dbd
Jalaj Pandey <62509462+jalaj-pandey@users.noreply.github.com>
2024-08-29 10:57:02 +0530

    Update Header.tsx

--- client/url-shortener/src/components/Header/Header.tsx
+++ client/url-shortener/src/components/Header/Header.tsx
@@ -1,15 +1,27 @@
-const Header = () => {
-  return (
-    <div className='bg-slate-900'>
-        <div className='container p-2 mx-auto'>
-            <nav className='py-5'>
-                <div className='text-base text-white'>
-                    UrlShortener
-                 </div>
-            </nav>
-        </div>
-    </div>
-  )
-}
+import React from 'react';
 
-export default Header
+const Header: React.FC = () => {
+  return (
+    <nav className="bg-slate-900 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* Logo */}
+        <div className="text-white text-2xl font-bold">
+          <a href="/" className="hover:text-yellow-300 transition duration-300">
+            TheShorty
+          </a>
+        </div>
+
+        <div>
+          <a
+            href="https://thejalaj.netlify.app/"
+            className="text-white text-lg hover:text-yellow-300 transition duration-300"
+          >
+            About Us
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
