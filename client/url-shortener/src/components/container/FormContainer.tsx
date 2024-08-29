6e61b52751b7ee1e8620ebe3dd774d5855ce5196
Jalaj Pandey <62509462+jalaj-pandey@users.noreply.github.com>
2024-08-29 11:53:00 +0530

    Update FormContainer.tsx

--- client/url-shortener/src/components/container/FormContainer.tsx
+++ client/url-shortener/src/components/container/FormContainer.tsx
@@ -52,7 +52,7 @@ const FormContainer:React.FunctionComponent<IFormContainerProps>= (props) => {
                 />
                 <button
                   type="submit"
-                  className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-yellow-300 rounded-lg hover:bg-yellow-500 hover:text-teal-600 border border-yellow-800 focus:ring-4 focus:outline-none focus:ring-yellow-300"
+                  className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-yellow-400 rounded-lg hover:bg-yellow-500 hover:text-teal-600 border border-yellow-800 focus:ring-4 focus:outline-none focus:ring-yellow-300"
                 >
                   Short
                 </button>
